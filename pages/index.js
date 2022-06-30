dad89c298b591b8ecad23f8237642e226bb104ba
vattenapa <kevin.einarsson@gmail.com>
2022-06-30 16:02:20 +0200

    added reproduction

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <meta name="foo" content="bar" />
+      </Head>
+      Hello world
+    </div>
+  );
+}
